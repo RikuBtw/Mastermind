b5da446410f3d47524283077949c203b6cf89bea
RikuBtw <rikuleixjun@gmail.com>
2016-11-26 20:53:12 +0100

    ...

--- modele/modeleAuthentification.php
+++ modele/modeleAuthentification.php
@@ -3,8 +3,8 @@
 class ModeleAuthentification{
 
   function verificationPseudo($pseudo, $password){
-    try{
-      $connexion=new PDO('mysql:host=localhost;dbname=E154817E','root','');
+    try
+      $connexion=new PDO('mysql:host=localhost;dbname=E154817E','E154817E','E154817E');
       $stmt = $connexion->prepare("SELECT pseudo, motDePasse from joueurs where joueurs.pseudo=?");
       $stmt->bindParam(1, $pseudo);
       $stmt->execute();
